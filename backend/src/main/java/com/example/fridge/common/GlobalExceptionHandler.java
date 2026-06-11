2cc9e47a7f291885a863306b68a4d4b135f434d3
940061 <qianjing4@gtht.com>
2026-06-11 13:10:54 +0800

    新增日志

--- backend/src/main/java/com/example/fridge/common/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/fridge/common/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.fridge.common;
 
 import javax.validation.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler({
             MethodArgumentNotValidException.class,
@@ -30,6 +34,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiResponse<Void> handleException(Exception ex) {
+        log.error("Unhandled exception", ex);
         return ApiResponse.fail("服务异常，请稍后再试");
     }
 }
